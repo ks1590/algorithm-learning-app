272e002e677a166a6167252d5dc7d763b2c20d51
ks1590 <69412467+ks1590@users.noreply.github.com>
2026-02-16 15:09:15 +0900

    feat: Add a new TopPage component as the application's landing page, updating routing and navigation to include it and a dedicated sorting page.

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { TreePage } from './pages/TreePage';
 import { SearchPage } from './pages/SearchPage';
 import { Layout } from './components/Layout';
 
+import { TopPage } from './pages/TopPage';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<SortingPage />} />
+          <Route path="/" element={<TopPage />} />
+          <Route path="/sorting" element={<SortingPage />} />
           <Route path="/tree" element={<TreePage />} />
           <Route path="/search" element={<SearchPage />} />
         </Route>
